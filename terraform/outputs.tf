af9157e4e0b7c421639fc55936e02f7cc5e657f6
Jonathan Wright <jon@than.io>
2024-02-20 23:23:46 +0000

    Remove redundant Cloudflare outputs from Terraform
    Remove the outputs related to the Cloudflare values from Terraform as
    those resources a no longer managed.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,13 +2,3 @@ output "tls_flux_public_key" {
   description = "The SSH Public Key for the Flux service when reconsiling and updating the Flux configuration"
   value       = tls_private_key.flux.public_key_pem
 }
-
-output "cloudflare_account" {
-  description = "The Account ID for Cloudflare"
-  value       = data.cloudflare_accounts.n3tuk.id
-}
-
-output "cloudflare_accounts" {
-  description = "The Account IDs for Cloudflare"
-  value       = data.cloudflare_accounts.n3tuk.accounts
-}
