655fdbddbb3ea3e2bcafac6756fd6f11e8abf74b
Jonathan Wright <jon@than.io>
2024-02-21 08:59:01 +0000

    Add simple Cloudflare Tunnel CNAME record
    Add a DNS record which acts as a simple CNAME alias to the Cloudflare
    Tunnel, which will be used instead for mapping services to the Tunnel.

--- terraform/k3s-cloudflare-tunnel.tf
+++ terraform/k3s-cloudflare-tunnel.tf
@@ -2,6 +2,10 @@ resource "random_password" "tunnel" {
   length = 64
 }
 
+resource "random_pet" "tunnel" {
+  length = 2
+}
+
 # Create and configure a tunnel in Cloudflare specific for this Kubernetes
 # Cluster, and then configure the namespace and additional configuration
 # directly into the Cluster
@@ -12,6 +16,15 @@ resource "cloudflare_tunnel" "kub3" {
 
   name   = "kub3-${local.cluster}"
   secret = base64sha256(random_password.tunnel.result)
+}
+
+resource "cloudflare_record" "kub3_tunnel" {
+  zone_id = data.cloudflare_zone.n3tuk["kub3.uk"].zone_id
+  name    = "${random_pet.tunnel.id}.tunnel"
+  comment = "Cloudflare Tunnel DNS alias for ${terraform.workspace}"
+
+  type  = "CNAME"
+  value = cloudflare_tunnel.kub3.cname
 }
 
 resource "kubernetes_namespace_v1" "cloudflare_system" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "tls_flux_public_key" {
   description = "The SSH Public Key for the Flux service when reconsiling and updating the Flux configuration"
   value       = tls_private_key.flux.public_key_pem
 }
+
+output "cloudflare_tunnel_cname" {
+  description = "The DNS CNAME to be used to point proxied traffic to to access hosted services in the Cluster"
+  value       = cloudflare_record.kub3_tunnel.hostname
+}
